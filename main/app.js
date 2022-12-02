15cdb8544d0f8c0538ca426273d1ff665129a1b3
Amaan Uddin <mohdamaanuddin100@gmail.com>
2022-12-03 01:00:34 +0300

    seventh commit

--- main/app.js
+++ main/app.js
@@ -1,1 +1,16 @@
 // document.body.style.zoom = .9
+const hamburger = document.querySelector(".ham-bar")
+const links = document.querySelector(".links")
+let counter = 0
+
+hamburger.addEventListener("click",() => {
+    if(!counter){
+        links.style.transform = "translateY(0)"
+        links.style.opacity = "1"
+        counter = 1
+    }else{
+        links.style.transform = "translateY(-10rem)"
+        links.style.opacity = "0"
+        counter = 0
+    }
+})
